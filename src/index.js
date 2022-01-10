576cd62abefdeb65418effb74f4b6f5ff2ccaee8
Stern-Ritter <fadeew.a@mail.ru>
2022-01-10 17:38:53 +0300

    Add Carousel class init to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles/pages/index.scss";
+import Carousel from "./carousel/carousel";
+
+const carousel = new Carousel({
+  mainSelector: "slider",
+  slidesCount: 2,
+});
+carousel.initialize();
